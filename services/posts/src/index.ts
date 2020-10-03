88c5db6f56a8bb9585b54d5209fae1be3a93c4b4
thiago <thiago.lopes.dev@gmail.com>
2020-10-03 11:59:15 -0300

    feat: add body-parser

--- services/posts/src/index.ts
+++ services/posts/src/index.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 import Routes from './routes/routes';
+import bodyParser from 'body-parser';
 const app =  express();
 
 
 const PORT = process.env.PORT || 8080;
-
+app.use(bodyParser.json());
 app.use(Routes);
 
 app.listen(PORT, () => {
