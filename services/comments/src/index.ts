0abd9fa071a411993c062d443adfbf7c6da57216
thiago <thiago.lopes.dev@gmail.com>
2020-10-06 13:47:47 -0300

    feat: add cors to project

--- services/comments/src/index.ts
+++ services/comments/src/index.ts
@@ -1,12 +1,14 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import Routes from './routes/routes';
+import cors from 'cors';
 
 const app = express();
 
 const PORT = process.env.PORT || 8070;
 
 app.use(bodyParser.json());
+app.use(cors());
 app.use(Routes);
 
 app.listen(PORT, () => {
